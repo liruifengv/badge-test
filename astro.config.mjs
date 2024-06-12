172fd8277f30b9d0f0161c75e8bf192a22173ec5
liruifengv <liruifeng1024@gmail.com>
2024-06-12 14:26:59 +0800

    init

---
+++ astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig, sharpImageService } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			customCss: ['./src/styles/custom.css'],
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+  image: {
+		domains: [],
+		service: sharpImageService(),
+	},
+});
